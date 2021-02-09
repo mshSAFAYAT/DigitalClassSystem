5972bc68d3029585e94b54cae71b2ebd20ecd2d7
Sheikh Moonwara Anjum Monisha <moonwaraanjum@iut-dhaka.edu>
2021-02-09 22:34:46 +0600

    join class filter

--- src/component/JoinClass.js
+++ src/component/JoinClass.js
@@ -3,54 +3,54 @@ import { useRef } from "react";
 import {Button,Card,Form,Alert, Container} from "react-bootstrap";
 import {useAuth} from "../context/AuthContext";
 import {BrowserRouter,Link,Switch, useHistory} from "react-router-dom";
+import { db } from "./../firebase";
 
 const JoinClass =()=>{
-    const classnameRef = useRef();
+    const [JoiningClass,setJClass] = useState(false);
     const codeRef = useRef();
-    const descriptionRef = useRef();
-
-    const confirmPasswordRef = useRef();
     const {updateEmail,updatePassword,currentUser} = useAuth();
     const history =useHistory();
 
     const [error, setError] = useState("");
     const [loading,setLoading] = useState(false);
+    let JClass =[];
 
     const handleJoin = async(e)=>{
-            history.push("/");
+        db.collection("Classes")
+        // .doc(codeRef)
+        .onSnapshot((querySnapshot)=>{
+            let allClasses=[]
+            querySnapshot.forEach((docRef)=>{
+                //console.log(docRef.data())
+                allClasses.push({
+                    id:docRef.id,
+                    data:docRef.data(),
+                });
+            });
+            //console.log(codeRef.current.value)
+            if(allClasses!=null){
+                 JClass=allClasses.filter(c=>c.data.ClassCode==codeRef.current.value )
+                setJClass(JClass)
+                console.log(JClass[0].id)
+            }
+            else console.log("no class")
+        },(error)=>{
+            console.log(error);
+        });
+        //let jid=JClass[0].id;
+        // db.collection("Classes").doc(
+        //     "FQJC3eI7feYYI7o3rvzY"
+        //     ).update({
+        //     members: db.FieldValue.arrayUnion(currentUser.uid),
+        // }).then(()=>{
+        //     alert("Joind in class " );
+        // })
+        //     history.push("/");
 
     }
     const handleSubmit =async(e)=>{
         e.preventDefault();
-
-    //     if(passwordRef.current.value !== confirmPasswordRef.current.value){
-    //       return  setError("Passwords do not match!");
-    //     }
-        
-    //         setLoading(true);
-    //         setError("");
-    //         const promises =[];
-    //         if(emailRef.current.value !== currentUser.email ){
-    //             promises.push(updateEmail(emailRef.current.value))
-    //         }
-    //         if(emailRef.current.value)
-    //         {
-    //             promises.push(updatePassword(passwordRef.current.value))
-    //         }
-    //         Promise.all(promises)
-    //         .then(()=>
-    //         {
-    //             history.push("/");
-    //         })
-    //         .catch((error)=>
-    //         {
-    //             setError(error)
-    //         })
-    //         .finally(()=>{
-    //             setLoading(false);
-    //         })
-            
-  
+              
      };
 
     return(
@@ -59,7 +59,7 @@ const JoinClass =()=>{
         <div className='w-100' style={{maxWidth:"400px"}}>
             <Card>
                 <Card.Body>
-                    <h2 className='text-center mb-4'>  Create Class</h2>
+                    <h2 className='text-center mb-4'>  Join Class</h2>
                     {error?(
                     <Alert variant="danger">{JSON.stringify(error)}</Alert>
 
@@ -70,7 +70,7 @@ const JoinClass =()=>{
                             <Form.Control ref={codeRef} type="code" required ></Form.Control>
                         </Form.Group>                       
                         <Button variant="Link" onClick={handleJoin} disabled={loading} classname="w-100" type="submit">Join</Button>
-                        <Button disabled={loading} classname="w-100" type="submit">Join</Button>
+                        {/* <Button disabled={loading} classname="w-100" type="submit">Join</Button> */}
                     </Form>
                     <div className="w-100 text-center mt-2"><Link to="/">Cancel</Link></div>
                 </Card.Body>
